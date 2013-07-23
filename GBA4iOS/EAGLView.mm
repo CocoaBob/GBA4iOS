0711bc33d96bb2668564ce6bfc8679163c8ba7f0
Riley Testut <rileytestut@gmail.com>
2013-07-23 02:54:14 -0500

    Created EAGLView class

---
+++ GBA4iOS/EAGLView.mm
@@ -0,0 +1,31 @@
+//
+//  EAGLView.m
+//  GBA4iOS
+//
+//  Created by Riley Testut on 7/23/13.
+//  Copyright (c) 2013 Riley Testut. All rights reserved.
+//
+
+#import "EAGLView.h"
+
+@implementation EAGLView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
