6e6a671d483897a3d305a91ed6466ada4897325b
Riley Testut <riley@rileytestut.com>
2014-02-10 05:19:37 -0600

    "Enabled" takes precedent over Cheat name

--- GBA4iOS/GBACheatManagerTableViewCell.m
+++ GBA4iOS/GBACheatManagerTableViewCell.m
@@ -40,6 +40,33 @@
     {
         self.detailTextLabel.hidden = NO;
     }
+    
+    [self layoutIfNeeded];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    if ([self.detailTextLabel.text isEqualToString:@""] || [self isEditing])
+    {
+        return;
+    }
+    
+    [self.detailTextLabel sizeToFit];
+    
+    CGFloat padding = self.textLabel.frame.origin.x;
+    CGFloat paddingMultiplier = 3;
+    
+    if (self.accessoryType != UITableViewCellAccessoryNone)
+    {
+        paddingMultiplier = 2;
+    }
+    
+    CGFloat maximumWidth = self.contentView.bounds.size.width - (self.detailTextLabel.bounds.size.width + padding * paddingMultiplier);
+    
+    self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, self.textLabel.frame.origin.y, maximumWidth, self.textLabel.frame.size.height);
+    self.detailTextLabel.frame = CGRectMake(CGRectGetMaxX(self.textLabel.frame) + padding, self.detailTextLabel.frame.origin.y, self.detailTextLabel.frame.size.width, self.detailTextLabel.frame.size.height);
 }
 
 @end
